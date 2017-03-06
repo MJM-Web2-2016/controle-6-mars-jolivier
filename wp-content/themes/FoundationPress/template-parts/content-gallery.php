3151acac4f0895a86248ece051cce9e7861b55b8
Julia-Olivier <julia.olivier.mjm@gmail.com>
2017-03-06 13:04:25 +0100

    Gallery + Style CSS

--- wp-content/themes/FoundationPress/template-parts/content-gallery.php
+++ wp-content/themes/FoundationPress/template-parts/content-gallery.php
@@ -12,14 +12,15 @@
 <div class="column column-block">
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
-  <?php get_template_part('template-parts/cover'); ?>
-	<header>
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<?php foundationpress_entry_meta(); ?>
-	</header>
-	<div class="entry-content">
-
-	</div>
+  <a href="<?php the_permalink(); ?>">
+    <figure class="gallery-item">
+      <figcaption>
+    		<h4><?php the_title(); ?></a></h4>
+    		<?php foundationpress_entry_meta(); ?>
+    	</figcaption>
+    <?php get_template_part('template-parts/cover'); ?>
+    </figure>
+  </a>
 	<footer>
 		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
 	</footer>
--- wp-content/themes/FoundationPress/template-parts/cover.php
+++ wp-content/themes/FoundationPress/template-parts/cover.php
@@ -1,5 +1,3 @@
 <?php if (has_post_thumbnail()): ?>
-		<div class="thumbnail">
 			<?php the_post_thumbnail('visuel-listing'); ?>
-		</div>
 	<?php endif; ?>
